0a7aa84cb57c803e686bab77c359e342ca38952a
Igor Moreira Alves <IgorAlvesBH>
2020-09-01 19:22:21 -0300

    Adicionando exemplo de parâmetro de rota.

--- pages/[index].jsx
+++ pages/[index].jsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import CovidAPIClient from '../src/client/CovidAPIClient';
 import Home from '../src/components/templates/Home';
+import { useRouter } from 'next/router';
 
 const HomePage = () => {
-  const [report, setReport] = useState();
-
+  const router = useRouter();
+  const [covidData, setCovidData] = useState();
+  const { index } = router.query;
   useEffect(() => {
-    if (!report) {
+    if (!covidData) {
       CovidAPIClient.getReportAllState().then((data) => {
-        setReport(data);
+        setCovidData(data);
       });
     }
-  });
+  }, [covidData]);
 
   return (
-    <Home dataTable={report}>
+    <Home dataTable={covidData}>
+      {index && <p>Seu valor de parâmetro é {index}</p>}
       <p>Abaixo segue um exemplo de implementação de <a href='https://pt-br.reactjs.org/docs/hooks-intro.html' target='blank' rel='noopener noreferrer'>React Hooks</a> com <a href='https://github.com/axios/axios' target='blank' rel='noopener noreferrer'>axios</a> utilizando a <a href='https://covid19-brazil-api.now.sh/' target='blank' rel='noopener noreferrer'>API Covid19 Brazil</a>.</p>
     </Home>
   );
--- pages/ssr.jsx
+++ pages/ssr.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import CovidAPIClient from '../src/client/CovidAPIClient';
 import Home from '../src/components/templates/Home';
 
-const SSR = ({ res }) => {
+const SSR = ({ covidData }) => {
   return (
-    <Home dataTable={res}>
+    <Home dataTable={covidData}>
       <p>Abaixo segue um exemplo de implementação de <a href='https://pt-br.reactjs.org/docs/hooks-intro.html' target='blank' rel='noopener noreferrer'>React Hooks</a> com <a href='https://github.com/axios/axios' target='blank' rel='noopener noreferrer'>axios</a> utilizando a <a href='https://covid19-brazil-api.now.sh/' target='blank' rel='noopener noreferrer'>API Covid19 Brazil</a>.</p>
     </Home>
   );
 };
 
 SSR.getInitialProps = async () => {
-  const res = await CovidAPIClient.getReportAllState();
-  return { res: res };
+  const covidData = await CovidAPIClient.getReportAllState();
+  return {
+    covidData
+  };
 };
 
 export default SSR;
