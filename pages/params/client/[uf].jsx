aa8c1b2f5397d609870f70ef9894393e335b4a58
Igor Moreira Alves <IgorAlvesBH>
2020-09-01 20:41:58 -0300

    Implementação parcial

---
+++ pages/params/client/[uf].jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import CovidAPIClient from '../../../src/client/CovidAPIClient';
+import Home from '../../../src/components/templates/Home';
+
+const HomePage = () => {
+  const [covidData, setCovidData] = useState();
+  const [uf, setUf] = useState();
+  const router = useRouter();
+  const { uf } = router.query;
+  useEffect(() => {
+    if (!covidData) {
+      CovidAPIClient.getReportAllState().then((data) => {
+        const filteredCovidState = uf ? { data: data.data.filter(obj => obj.uf === uf.toLocaleUpperCase()) } : data;
+        console.log(uf)
+        console.log('carai', filteredCovidState)
+        setCovidData(filteredCovidState);
+      });
+    }
+  }, [uf]);
+
+  return (
+    <Home dataTable={covidData}>
+      <p>Abaixo segue um exemplo de implementação de <a href='https://pt-br.reactjs.org/docs/hooks-intro.html' target='blank' rel='noopener noreferrer'>React Hooks</a> com <a href='https://github.com/axios/axios' target='blank' rel='noopener noreferrer'>axios</a> utilizando a <a href='https://covid19-brazil-api.now.sh/' target='blank' rel='noopener noreferrer'>API Covid19 Brazil</a>.</p>
+    </Home>
+  );
+};
+
+export default HomePage;
--- src/components/molecules/Menu/index.jsx
+++ src/components/molecules/Menu/index.jsx
@@ -31,6 +31,20 @@ const Menu = () => {
       </Style.ListItem>
       <Style.ListItem>
         <MenuLink
+          url='/params/ssr/ac'
+          text='Render no server com param'
+          className={router.pathname === '/params/ssr/ac' ? 'active' : ''}
+        />
+      </Style.ListItem>
+      <Style.ListItem>
+        <MenuLink
+          url='/params/client/ac'
+          text='Render no client com param'
+          className={router.pathname === '/params/client/ac' ? 'active' : ''}
+        />
+      </Style.ListItem>
+      <Style.ListItem>
+        <MenuLink
           url='/about'
           text='Sobre'
           className={router.pathname === '/about' ? 'active' : ''}
